93650e491401e9e28e0df8dc3603786ed6c89362
honeycomb80 <honeycomb8080@gmail.com>
2014-05-06 10:58:13 -0700

    created new app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CodeMvp::Application.config.secret_key_base = '35346f9d2f746fd08321524930b47b0fa6460575b95cd8b3540c55afcc1c55369c3f1c4b89550000a1235da8c8161900d6d5cf402739890439567e12d19e1c66'
