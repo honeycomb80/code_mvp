6abbbbeeb7e7b9a454eee0fb5921799d8f154c50
honeycomb80 <honeycomb8080@gmail.com>
2014-05-06 11:44:04 -0700

    raked db:migrate, seeded file, raked db:seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+prompts = PromptsData.create([
+	{prompt: "st.          = 2", answer: "length"},
+	{prompt: "stri.          = 4", answer: "length"},
+	{prompt: "string.          = gnirts", answer: "reverse"},
+	{prompt: "gnirts.          = string", answer: "reverse"},
+	{prompt: "string.          = STRING", answer: "upcase"},
+	{prompt: "Gnirts.          = GNIRTS", answer: "upcase"},
+	{prompt: "STRING.          = string", answer: "downcase"},
+	{prompt: "Gnirts.          = gnirts", answer: "downcase"}
+	])
