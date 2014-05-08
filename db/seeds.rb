04507039e6667a279a574f88af11f7db326ce6ce
honeycomb80 <honeycomb8080@gmail.com>
2014-05-08 14:51:10 -0700

    raked new seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,12 +7,12 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 prompts = PromptsData.create([
-	{prompt: "st. = 2", answer: "length"},
-	{prompt: "stri. = 4", answer: "length"},
-	{prompt: "string. = gnirts", answer: "reverse"},
-	{prompt: "gnirts. = string", answer: "reverse"},
-	{prompt: "string. = STRING", answer: "upcase"},
-	{prompt: "Gnirts. = GNIRTS", answer: "upcase"},
-	{prompt: "STRING. = string", answer: "downcase"},
-	{prompt: "Gnirts. = gnirts", answer: "downcase"}
+	{prompt_1: "st.", prompt_2: "= 2", answer: "length"},
+	{prompt_1: "stri.", prompt_2: "= 4", answer: "length"},
+	{prompt_1: "string.", prompt_2: "= gnirts", answer: "reverse"},
+	{prompt_1: "gnirts.", prompt_2: "= string", answer: "reverse"},
+	{prompt_1: "string.", prompt_2: "= STRING", answer: "upcase"},
+	{prompt_1: "Gnirts.", prompt_2: "= GNIRTS", answer: "upcase"},
+	{prompt_1: "STRING.", prompt_2: "= string", answer: "downcase"},
+	{prompt_1: "Gnirts.", prompt_2: "= gnirts", answer: "downcase"}
 	])
