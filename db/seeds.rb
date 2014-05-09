ae59ba082659bb081cbe7af669bfc09c16b6b64b
honeycomb80 <honeycomb8080@gmail.com>
2014-05-08 23:13:05 -0700

    finished some css, reset the db

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,12 +7,12 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 prompts = PromptsData.create([
-	{prompt_1: "st.", prompt_2: "= 2", answer: "length"},
-	{prompt_1: "stri.", prompt_2: "= 4", answer: "length"},
-	{prompt_1: "string.", prompt_2: "= gnirts", answer: "reverse"},
-	{prompt_1: "gnirts.", prompt_2: "= string", answer: "reverse"},
-	{prompt_1: "string.", prompt_2: "= STRING", answer: "upcase"},
-	{prompt_1: "Gnirts.", prompt_2: "= GNIRTS", answer: "upcase"},
-	{prompt_1: "STRING.", prompt_2: "= string", answer: "downcase"},
-	{prompt_1: "Gnirts.", prompt_2: "= gnirts", answer: "downcase"}
+	{prompt_1: "st", prompt_2: "= 2", answer: ".length"},
+	{prompt_1: "stri", prompt_2: "= 4", answer: ".length"},
+	{prompt_1: "string", prompt_2: "= gnirts", answer: ".reverse"},
+	{prompt_1: "gnirts", prompt_2: "= string", answer: ".reverse"},
+	{prompt_1: "string", prompt_2: "= STRING", answer: ".upcase"},
+	{prompt_1: "Gnirts", prompt_2: "= GNIRTS", answer: ".upcase"},
+	{prompt_1: "STRING", prompt_2: "= string", answer: ".downcase"},
+	{prompt_1: "Gnirts", prompt_2: "= gnirts", answer: ".downcase"}
 	])
